1b0d4f79ca9f1420a554dfc825d857c1a67c104e
Abhishek Singh <abhishekrathore1806.com>
2024-03-03 23:08:07 +0530

    Completed SignUp Functionality still Google Authentication is remaining

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,7 +1,44 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import { Label,TextInput,Button } from 'flowbite-react'
+import { Label,TextInput,Button ,Spinner,Alert} from 'flowbite-react'
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 export default function SignUp() {
+    const [formData, setFormData] = useState({});
+    const [errorMessage, setErrorMessage] = useState(null);
+    const [loading, setLoading] = useState(false);
+    const navigate = useNavigate();
+    const handleChange = (e) => {
+        // console.log(e.target.value);
+        setFormData({ ...formData, [e.target.id]: e.target.value.trim() });
+      };
+      const handleSubmit = async (e) => {
+        e.preventDefault();
+        if (!formData.username || !formData.email || !formData.password) {
+          return setErrorMessage('Please fill out all fields.');
+        }
+        try {
+          setLoading(true);
+          setErrorMessage(null);
+          const res = await fetch('/api/auth/signup', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(formData),
+          });
+          const data = await res.json();
+          if (data.success === false) {
+            return setErrorMessage(data.message);
+          }
+          setLoading(false);
+          if(res.ok) {
+            navigate('/sign-in');
+          }
+        } catch (error) {
+          setErrorMessage(error.message);
+          setLoading(false);
+        }
+      };
+
   return <div className='min-h-screen mt-20'>
     <div className='flex p-3 max-w-3xl mx-auto flex-col md:flex-row md:items-center gap-5'>
     {/* left */}
@@ -22,14 +59,14 @@ export default function SignUp() {
     </div>
     {/* right */}
     <div className='flex-1'>
-    <form className='flex flex-col gap-4' /*onSubmit={handleSubmit}*/>
+    <form className='flex flex-col gap-4' onSubmit={handleSubmit}>
             <div>
               <Label value='Your username' />
               <TextInput
                 type='text'
                 placeholder='Username'
                 id='username'
-                // onChange={handleChange}
+                onChange={handleChange}
               />
             </div>
             <div>
@@ -38,7 +75,7 @@ export default function SignUp() {
                 type='email'
                 placeholder='name@company.com'
                 id='email'
-                // onChange={handleChange}
+                onChange={handleChange}
               />
             </div>
             <div>
@@ -47,7 +84,7 @@ export default function SignUp() {
                 type='password'
                 placeholder='Password'
                 id='password'
-                // onChange={handleChange}
+                onChange={handleChange}
               />
             </div>
             <Button
@@ -55,15 +92,15 @@ export default function SignUp() {
               type='submit'
             //   disabled={loading}
             >
-            Sign Up
-              {/* {loading ? (
+            
+              {loading ? (
                 <>
                   <Spinner size='sm' />
                   <span className='pl-3'>Loading...</span>
                 </>
               ) : (
                 'Sign Up'
-              )} */}
+              )}
             </Button>
             {/* <OAuth /> */}
           </form>
@@ -72,6 +109,11 @@ export default function SignUp() {
             <Link to='/sign-in' className='text-blue-500'>
               Sign In
             </Link>
+            {errorMessage && (
+            <Alert className='mt-5' color='failure'>
+              {errorMessage}
+            </Alert>
+          )}
           </div>
     </div>
     </div>
