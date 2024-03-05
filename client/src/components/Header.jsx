5a3886f924a10bdf90d9bd48891fbb374a89d686
Abhishek Singh <abhishekrathore1806.com>
2024-03-05 14:13:11 +0530

    Updated Header with User Data using Redux by retrieving information from user state

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -3,8 +3,11 @@ import { Avatar, Button, Dropdown, Navbar, NavbarToggle, TextInput } from 'flowb
 import { Link, useLocation, useNavigate } from 'react-router-dom';
 import { AiOutlineSearch } from 'react-icons/ai';
 import { FaMoon, FaSun } from 'react-icons/fa';
+import { useSelector } from 'react-redux';
 export default function Header() {
   const path = useLocation().pathname;
+  const { currentUser } = useSelector((state) => state.user);
+
   return (
     <Navbar className='border-b-2'>
       <Link
@@ -39,11 +42,31 @@ export default function Header() {
           {/* {theme === 'light' ? <FaSun /> : <FaMoon />} */}
           <FaMoon />
         </Button>
-        <Link to='/sign-in'>
+        {currentUser ? (
+          <Dropdown
+            arrowIcon={false}
+            inline
+            label={
+              <Avatar alt='user' img={currentUser.profilePicture} rounded />
+            }
+          >
+            <Dropdown.Header>
+              <span className='block text-sm'>@{currentUser.username}</span>
+              <span className='block text-sm font-medium truncate'>{currentUser.email}</span>
+            </Dropdown.Header>
+            <Link to={'/dashboard?tab=profile'}>
+              <Dropdown.Item>Profile</Dropdown.Item>
+            </Link>
+            <Dropdown.Divider />
+            <Dropdown.Item>Sign out</Dropdown.Item>
+          </Dropdown>
+        ) : (
+          <Link to='/sign-in'>
             <Button gradientDuoTone='purpleToBlue' outline>
               Sign In
             </Button>
           </Link>
+        )}
           <NavbarToggle></NavbarToggle>
           </div>
           <Navbar.Collapse>
