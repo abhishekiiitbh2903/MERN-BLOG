d22367593f8b03f76c9411b680c2581bc37110c6
Abhishek Singh <abhishekrathore1806.com>
2024-03-06 01:35:37 +0530

    Completed Delete Functionality

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,10 +1,11 @@
 import express from 'express';
 const router = express.Router();
-import { test,updateUser } from '../controllers/user.controller.js';
+import { deleteUser,test,updateUser } from '../controllers/user.controller.js';
 import { verifyToken } from '../utils/verifyUser.js';
 // router.get('/test',(req,res)=>{
 //     res.json({msg:"hello World"})
 // });
 router.get('/test',test);
 router.put('/update/:userId', verifyToken,updateUser);
+router.delete('/delete/:userId', verifyToken,deleteUser);
 export default router
