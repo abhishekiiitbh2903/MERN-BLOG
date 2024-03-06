193af99f84b0fe7d5688f00490b0617c1c588308
Abhishek Singh <abhishekrathore1806.com>
2024-03-06 15:25:29 +0530

    Completed (Create a post page UI)

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,6 +8,8 @@ import Dashboard from './pages/Dashboard'
 import Projects from './pages/Projects'
 import Header from './components/Header'
 import Footer from './components/Footer'
+import OnlyAdminPrivateRoute from './components/OnlyAdminPrivateRoute';
+import CreatePost from './pages/CreatePost';
 import PrivateRoute from './components/PrivateRoute'
 const App = () => {
   return (
@@ -20,6 +22,9 @@ const App = () => {
         <Route path='/sign-up' element={<SignUp />} />
         <Route element={<PrivateRoute />}>
           <Route path='/dashboard' element={<Dashboard />} />
+        </Route>
+        <Route element={<OnlyAdminPrivateRoute />}>
+          <Route path='/create-post' element={<CreatePost />} />
         </Route>
         <Route path='/projects' element={<Projects />} />
       </Routes>
--- client/src/components/DashProfile.jsx
+++ client/src/components/DashProfile.jsx
@@ -11,6 +11,7 @@ import {
 import { app } from '../firebase';
 import { CircularProgressbar } from 'react-circular-progressbar';
 import 'react-circular-progressbar/dist/styles.css';
+import { Link } from 'react-router-dom';
 import {
   updateStart,
   updateSuccess,
@@ -24,7 +25,7 @@ import { useDispatch } from 'react-redux';
 import { HiOutlineExclamationCircle } from 'react-icons/hi';
 export default function DashProfile() {
     const dispatch = useDispatch();
-    const { currentUser,error } = useSelector((state) => state.user);
+    const { currentUser,error,loading} = useSelector((state) => state.user);
     const [imageFile,setImageFile]=useState(null);
     const [imageFileUrl, setImageFileUrl] = useState(null);
     const [imageFileUploadProgress, setImageFileUploadProgress] = useState(null);
@@ -207,9 +208,23 @@ export default function DashProfile() {
           onChange={handleChange}
         />
         <TextInput type='password' id='password' placeholder='password' onChange={handleChange}/>
-        <Button type='submit' gradientDuoTone='purpleToBlue' outline>
-            Update
+        <Button type='submit' gradientDuoTone='purpleToBlue'
+        outline
+        disabled={loading || imageFileUploading}
+        >
+        {loading ? 'Loading...' : 'Update'}
         </Button>
+        {currentUser.isAdmin && (
+          <Link to={'/create-post'}>
+            <Button
+              type='button'
+              gradientDuoTone='purpleToPink'
+              className='w-full'
+            >
+              Create a post
+            </Button>
+          </Link>
+        )}
       </form>
       <div className="text-red-500 flex justify-between mt-5">
         <span onClick={() => setShowModal(true)} className='cursor-pointer'>Delete Account</span>
