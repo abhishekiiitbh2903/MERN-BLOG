f015b4cbed8a03da8f297b0f4bc861001632c2d9
Abhishek Singh <abhishekrathore1806.com>
2024-03-06 14:56:48 +0530

    Added Admin Functionality and populating cookie with isadmin value too

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -68,7 +68,7 @@ export default function Header() {
             }
           >
             <Dropdown.Header>
-              <span className='block text-sm'>@{currentUser.username}</span>
+              <span className='block text-sm'>{currentUser.username}</span>
               <span className='block text-sm font-medium truncate'>{currentUser.email}</span>
             </Dropdown.Header>
             <Link to={'/dashboard?tab=profile'}>
